52479334263b99772ae5241d10204f9b2048d222
Xuelin Wang <xuelin.wang@gmail.com>
2016-02-24 21:45:31 -0600

    test

--- src/cloudxmark/web.clj
+++ src/cloudxmark/web.clj
@@ -1,5 +1,6 @@
 (ns cloudxmark.web
-  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+  (:require
+            [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
@@ -15,21 +16,14 @@
     (:gen-class)
     )
 
-(defn ping [id]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body (str "Hello, " id)})
-
 (defroutes routes
-  (GET "/user/:id" [id] (ping id))
-  (GET "/getBookmarks" [] (get-bookmarks))
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (def application (wrap-defaults routes site-defaults))
 
 (defn -main [& [port]]
-      (migrate)
+;;      (migrate)
       (let [port (Integer. (or port (env :port) 5000))]
           (jetty/run-jetty application {:port port :join? false})))
 
