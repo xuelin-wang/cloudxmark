ee6b6068e11a173af5484395bbb887231ffa36dc
Xuelin Wang <xuelin.wang@gmail.com>
2016-02-24 21:51:40 -0600

    test

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject cloudxmark "1.0.0-SNAPSHOT"
   :description "Cloudxmark web app"
   :url "http://cloudxmark.com"
-  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
